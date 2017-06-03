f3a88a461d3a45727d8269a6f6378881a66a525c
[Yashwanth Banda] <bandayashwanth@gmail.com>
2017-06-02 22:48:13 -0400

    Updated Renderers in Component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,9 +7,29 @@ import React,{Component} from 'react';
 // }
 
 class SearchBar extends Component {
-    render(){
+    constructor(prop){
+        super(prop);
 
-        return <input/>;
+        this.state={inputValue:'Default Value'};
     }
+
+    render(){
+        // return <input onChange={this.onInputChange}/>;
+            return (
+            <div>
+                <input 
+                    value ={this.state.inputValue}
+                    onChange ={ 
+                    event => {
+                            this.setState({inputValue:event.target.value});
+                            console.log(this.state.inputValue);
+                             }
+                         }/>
+            </div>
+            );
+    }
+    // onInputChange(event){
+    //     console.log(event);
+    // }
 }
 export default SearchBar;
